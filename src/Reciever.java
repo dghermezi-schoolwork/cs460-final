8fd869c7d1f749a80466378fe4f7190be29ed0d2
David Ghermezi <davidghermezi@gmail.com>
2016-11-30 21:34:40 -0800

    removed unused imports and throw declarations

--- src/Reciever.java
+++ src/Reciever.java
@@ -3,16 +3,11 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.X509EncodedKeySpec;
 
 public class Reciever {
 	
-	public static void main(String[] args) throws InvalidKeySpecException, NoSuchAlgorithmException, ClassNotFoundException, IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		
 		if (args.length != 1) {
 			System.out.println("Incorrect usage of program! Must pass one command line argument!");
